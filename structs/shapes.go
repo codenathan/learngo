396d910a214404f366acde6184fd64a292f61dce
S Nathan <codenathan@codenathan.com>
2022-02-10 13:50:39 +0000

    updated testarea

--- structs/shapes.go
+++ structs/shapes.go
@@ -26,3 +26,12 @@ func (c Circle) Area() float64 {
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
